6ca6ad3eca6759a339210284aeec7126692c06c8
lirancr <lirancr@gmail.com>
2015-09-11 14:21:19 +0300

    Added javadoc comments to Utils methods

--- app/src/main/java/org/denovogroup/rangzen/backend/Utils.java
+++ app/src/main/java/org/denovogroup/rangzen/backend/Utils.java
@@ -12,6 +12,12 @@ import java.util.TimeZone;
  */
 public class Utils {
 
+    /** Convert a timestemp in milliseconds into a human readable string format
+     *
+     * @param milli time in milliseconds to be converted
+     * @return human readable string representation of the time stamp using UTC timezone
+     * and formatted as yyyy-MM-dd HH:mm:ss
+     */
     public static String convertTimestampToDateString(long milli){
         Date date = new Date(milli);
         SimpleDateFormat sourceFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -19,12 +25,24 @@ public class Utils {
         return sourceFormat.format(date);
     }
 
+    /** convert numeric value (assumed to represent dp size units) into px units
+     * based on the device's screen density.
+     * @param dp value in dp to be converted
+     * @param context the context to get device settings with
+     * @return value in px after conversion from dp
+     */
     public static int dpToPx(int dp, Context context) {
         float density = context.getResources().getDisplayMetrics().density;
         int px = Math.round(dp * density);
         return px;
     }
 
+    /** convert numeric value (assumed to represent px size units) into dp units
+     * based on the device's screen density.
+     * @param px value in dp to be converted
+     * @param context the context to get device settings with
+     * @return value in dp after conversion from px
+     */
     public static int pxToDp(int px, Context context) {
         float density = context.getResources().getDisplayMetrics().density;
         int dp = Math.round(px / density);
