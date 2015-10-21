b7982a50c6ec71bcd0f7d16205290d8003812d0f
lirancr <lirancr@gmail.com>
2015-10-21 19:54:39 +0300

    Add multidex support for pre-lolipop version of android

--- app/src/main/java/org/denovogroup/rangzen/ui/RangzenApplication.java
+++ app/src/main/java/org/denovogroup/rangzen/ui/RangzenApplication.java
@@ -1,16 +1,28 @@
 package org.denovogroup.rangzen.ui;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 /**
  * Created by Liran on 9/1/2015.
  */
-public class RangzenApplication extends Application{
+public class RangzenApplication extends Application {
 
     private static String TAG = "RangzenApplication";
 
     @Override
     public final void onCreate() {
         super.onCreate();
+    }
+
+    /**
+     * This is part of the multidexing support for pre-lolipop devices
+     * it may removed once multidexing no longer reuqired
+     */
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
