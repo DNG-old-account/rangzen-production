2878b8d12b1ef2d4c2bd38d237907fa254cf6226
lirancr <lirancr@gmail.com>
2015-10-23 12:05:10 +0300

    make pulled messages trigger new-unread messages notification. made installation object duplicate on parse in case real installation object is not created due to a bug in parse.

--- app/src/main/java/org/denovogroup/rangzen/ui/RangzenApplication.java
+++ app/src/main/java/org/denovogroup/rangzen/ui/RangzenApplication.java
@@ -3,7 +3,10 @@ package org.denovogroup.rangzen.ui;
 import android.app.Application;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Handler;
 import android.os.StrictMode;
 import android.support.multidex.MultiDex;
 import android.util.Log;
@@ -17,15 +20,19 @@ import com.parse.ParseInstallation;
 import com.parse.ParseObject;
 import com.parse.ParsePush;
 import com.parse.ParseQuery;
+import com.parse.PushService;
+import com.parse.SaveCallback;
 
 import io.fabric.sdk.android.Fabric;
 import org.denovogroup.rangzen.backend.FriendStore;
 import org.denovogroup.rangzen.backend.MessageStore;
+import org.denovogroup.rangzen.backend.ReadStateTracker;
 import org.denovogroup.rangzen.backend.StorageBase;
 import org.denovogroup.rangzen.beta.CustomParsePushReceiver;
 import org.denovogroup.rangzen.beta.NetworkHandler;
 import org.denovogroup.rangzen.objects.RangzenMessage;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -54,9 +61,54 @@ public class RangzenApplication extends Application{
         thisInstallation.put("UUID",""+UUID.nameUUIDFromBytes(BluetoothAdapter.getDefaultAdapter().getAddress().getBytes()));
         thisInstallation.put("publicId",store.getPublicDeviceIDString());
         String myPublicId = store.getPublicDeviceIDString();
-        thisInstallation.put("readableId",myPublicId.substring(myPublicId.length()-9));
+        thisInstallation.put("readableId", myPublicId.substring(myPublicId.length() - 9));
+        //save this installation in the native way
         thisInstallation.saveInBackground();
 
+        //check if a backup has already been made for this installation and create one if not
+        createAndSaveInstallationDuplicate(thisInstallation);
+
+        //get messages, friends and time schedule from parse or log UserData if first run
+        getAndSaveUserData();
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+    private ParseObject createInstallationDuplicate(ParseInstallation installation){
+        ParseObject copy = new ParseObject("InstallationDuplicate");
+
+        for(String key : installation.keySet()){
+            copy.put(key, installation.get(key));
+        }
+        return copy;
+    }
+
+    private void createAndSaveInstallationDuplicate(final ParseInstallation installation){
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("InstallationDuplicate");
+        query.whereEqualTo("installationId", ParseInstallation.getCurrentInstallation().getInstallationId());
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> list, ParseException e) {
+                if (e == null) {
+                    if (list.isEmpty()) {
+                        /**create a duplicate of the installation and save as a public object on parse, this is used as a backup
+                         in the event that installation was not saved for some reason**/
+                        ParseObject installationCopy = createInstallationDuplicate(installation);
+                        installationCopy.saveInBackground();
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void getAndSaveUserData(){
         /**Try to get friends and messages from parse and register if first run*/
         ParseQuery<ParseObject> query = ParseQuery.getQuery("UserData");
         query.whereEqualTo("installationId", ParseInstallation.getCurrentInstallation().getInstallationId());
@@ -64,11 +116,10 @@ public class RangzenApplication extends Application{
             public void done(List<ParseObject> installationsList, ParseException e) {
                 if (e == null) {
                     if (installationsList != null && installationsList.size() > 0) {
-
                         //get friends
                         FriendStore fStore = new FriendStore(getApplicationContext(), StorageBase.ENCRYPTION_DEFAULT);
                         ArrayList<String> friends = (ArrayList<String>) installationsList.get(0).get("friends");
-                        if(friends != null) {
+                        if (friends != null) {
                             for (String friend : friends) {
                                 byte[] bytes = fStore.base64ToBytes(friend);
                                 // Try to add the friend to the FriendStore, if they're not null.
@@ -86,22 +137,39 @@ public class RangzenApplication extends Application{
                         List<RangzenMessage> receivedMessages = CustomParsePushReceiver.parseMessage(pushedContent);
                         if (receivedMessages != null) {
                             MessageStore mStore = new MessageStore(getApplicationContext(), StorageBase.ENCRYPTION_DEFAULT);
+                            boolean receivedNew = false;
                             for (RangzenMessage receivedMessage : receivedMessages) {
-                                mStore.addMessage(receivedMessage.text, receivedMessage.priority, true, receivedMessage.mId);
+                                if(!mStore.contains(receivedMessage.text)) {
+                                    receivedNew = true;
+                                    mStore.addMessage(receivedMessage.text, receivedMessage.priority, true, receivedMessage.mId);
+                                    ReadStateTracker.setReadState(getApplicationContext(),receivedMessage.text, false);
+                                }
+
+                                if(receivedNew){
+                                    Handler handler = new Handler();
+                                    handler.postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Intent intent = new Intent();
+                                            intent.setAction(MessageStore.NEW_MESSAGE);
+                                            getApplicationContext().sendBroadcast(intent);
+                                        }
+                                    }, 500);
+                                }
                             }
                         }
 
                         //get update schedule
-                        ArrayList<Integer> schedule  = (ArrayList<Integer>) installationsList.get(0).get("updateHours");
-                        if(schedule != null){
+                        ArrayList<Integer> schedule = (ArrayList<Integer>) installationsList.get(0).get("updateHours");
+                        if (schedule != null) {
                             Set<String> set = new HashSet<String>();
-                            for (Integer in : schedule){
+                            for (Integer in : schedule) {
                                 set.add(in.toString());
                             }
 
                             SharedPreferences prefs = getSharedPreferences("schedule", MODE_PRIVATE);
                             SharedPreferences.Editor editor = prefs.edit();
-                            editor.putStringSet("schedule",set);
+                            editor.putStringSet("schedule", set);
                             editor.commit();
 
                             NetworkHandler.getInstance(getApplicationContext());
@@ -115,7 +183,7 @@ public class RangzenApplication extends Application{
                         FriendStore store = new FriendStore(getApplicationContext(), StorageBase.ENCRYPTION_DEFAULT);
                         ParseObject userdata = new ParseObject("UserData");
                         userdata.put("installationId", ParseInstallation.getCurrentInstallation().getInstallationId());
-                        userdata.put("UUID", ""+UUID.nameUUIDFromBytes(BluetoothAdapter.getDefaultAdapter().getAddress().getBytes()));
+                        userdata.put("UUID", "" + UUID.nameUUIDFromBytes(BluetoothAdapter.getDefaultAdapter().getAddress().getBytes()));
                         userdata.put("publicId", store.getPublicDeviceIDString());
                         userdata.saveInBackground();
                     }
@@ -126,9 +194,4 @@ public class RangzenApplication extends Application{
         });
     }
 
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        MultiDex.install(this);
-    }
 }
