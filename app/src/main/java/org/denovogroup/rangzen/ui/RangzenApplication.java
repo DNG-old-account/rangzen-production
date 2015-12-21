77b43cadcdd4129557d7efb814908211a9fa551e
lirancr <lirancr@gmail.com>
2015-12-21 17:39:00 +0200

    - remove unused imports - removed multidexing - removed unused classes

--- app/src/main/java/org/denovogroup/rangzen/ui/RangzenApplication.java
+++ app/src/main/java/org/denovogroup/rangzen/ui/RangzenApplication.java
@@ -2,9 +2,6 @@ package org.denovogroup.rangzen.ui;
 
 import android.app.Application;
 import android.content.Context;
-import android.support.multidex.MultiDex;
-import android.telephony.TelephonyManager;
-import android.util.Log;
 
 /**
  * Created by Liran on 9/1/2015.
@@ -18,16 +15,6 @@ public class RangzenApplication extends Application {
     public final void onCreate() {
         super.onCreate();
         context = this;
-    }
-
-    /**
-     * This is part of the multidexing support for pre-lolipop devices
-     * it may removed once multidexing no longer reuqired
-     */
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        MultiDex.install(this);
     }
 
     public static Context getContext(){
