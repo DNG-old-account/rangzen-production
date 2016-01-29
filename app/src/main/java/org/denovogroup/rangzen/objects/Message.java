9e5fd2a20ba26ecb12882ea770bb01c7f88134d3
lirancr <lirancr@gmail.com>
2016-01-29 15:31:45 +0200

    Refactor project from Rangzen to Murmur

--- app/src/main/java/org/denovogroup/rangzen/objects/Message.java
+++
@@ -1,7 +0,0 @@
-package org.denovogroup.rangzen.objects;
-
-/**
- * Created by Liran on 12/20/2015.
- */
-public abstract class Message {
-}
--- app/src/main/java/org/denovogroup/rangzen/ui/FragmentBackHandler.java
+++
@@ -1,8 +0,0 @@
-package org.denovogroup.rangzen.ui;
-
-/**
- * Created by Liran on 1/13/2016.
- */
-public interface FragmentBackHandler {
-    boolean onBackPressed();
-}
